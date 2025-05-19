4e5491e72caeb68d81a7a3486b1d7cd3b9df37f4
Sawa8811 <sawa88101@gmail.com>
2025-05-19 17:37:45 +0900

    🎉 上傳 intro-web 專案初版

---
+++ src/main/java/com/Sawa/introweb/IntroWebApplication.java
@@ -0,0 +1,15 @@
+package com.Sawa.introweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Sawa
+ * @version 1.0
+ */
+@SpringBootApplication
+public class IntroWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IntroWebApplication.class,args);
+    }
+}
